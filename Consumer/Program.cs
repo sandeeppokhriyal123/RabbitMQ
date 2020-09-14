62acc7b1a76a90e0dca7facf140dacd1ef869bc6
Sandeep Pokhriyal <sandeep.pokhriyal2@synapse.com>
2020-09-14 11:49:56 +0530

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Message Received {0} ", message);
+            };
+
+            channel.BasicConsume("BasicTest", true, consumer);
+            
+        }
+
+       
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName="localhost"
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+            string message = "Sandeep's hello world";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "BasicTest", null, body);
+            Console.WriteLine("Message has been sent {0} ", message);
+
+        }
+    }
+}
